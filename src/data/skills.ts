2fffe3b3f4d565973a706f939d89f27e3f915354
MohmedhKA <kamohmedh2006@gmail.com>
2026-06-05 22:33:27 +0530

    UPDATES ON SKILL AND STYLE- v2.1

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -6,15 +6,15 @@ export interface Skill {
 }
 
 export const skills: Skill[] = [
-  { label: "Network Security",   angle: -35,  distance: 0.72, depth: 0.80 },
-  { label: "Cryptography",       angle: 105,  distance: 0.74, depth: 0.90 },
+  { label: "Network Security",   angle: -25,  distance: 0.72, depth: 0.80 },
+  { label: "Cryptography",       angle: 120,  distance: 0.64, depth: 0.90 },
   { label: "Linux",              angle: 150,  distance: 0.60, depth: 0.40 },
   { label: "Python",             angle: -160, distance: 0.70, depth: 1.00 },
-  { label: "Wireshark",          angle: -110, distance: 0.68, depth: 0.70 },
-  { label: "Burp Suite",         angle:  -70, distance: 0.76, depth: 1.20 },
-  { label: "Blockchain",         angle: 70,  distance: 0.65, depth: 0.85 },
-  { label: "Assembly",           angle: 40,  distance: 0.67, depth: 0.82 },
-  { label: "Metasploit",         angle: 0, distance: 0.67, depth: 0.82},
-  { label: "Rest API",         angle: -90, distance: 0.67, depth: 0.82},
+  { label: "Wireshark",          angle: -130, distance: 0.68, depth: 0.70 },
+  { label: "Burp Suite",         angle: -60, distance: 0.76, depth: 1.20 },
+  { label: "Blockchain",         angle: 85,  distance: 0.65, depth: 1.00 },
+  { label: "Assembly",           angle: 60,  distance: 0.67, depth: 0.82 },
+  { label: "Metasploit",         angle: 30, distance: 0.67, depth: 0.82},
+  { label: "Rest API",           angle: -90, distance: 0.67, depth: 0.82},
 
 ];
